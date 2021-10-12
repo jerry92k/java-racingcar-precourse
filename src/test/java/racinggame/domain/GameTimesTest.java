da07cfe42f3556423c6782c38d514d23c50d691b
jerry92k <jens92.k@gmail.com>
2021-10-12 19:59:25 +0900

    style(GameTimeTest) : 포맷팅

--- src/test/java/racinggame/domain/GameTimesTest.java
+++ src/test/java/racinggame/domain/GameTimesTest.java
@@ -11,28 +11,28 @@ import racinggame.domain.GameTimes;
 public class GameTimesTest {
 
 	@ParameterizedTest
-	@ValueSource(ints={1,345,5,765,7})
-	public void 게임_횟수_객체_생성(int totalTimes){
+	@ValueSource(ints = {1, 345, 5, 765, 7})
+	public void 게임_횟수_객체_생성(int totalTimes) {
 		GameTimes gameTimes = new GameTimes(totalTimes);
 		assertThat(gameTimes.getTotalTimes()).isEqualTo(totalTimes);
 	}
 
 	@ParameterizedTest
-	@ValueSource(ints={0,-1,-3436535})
-	public void 유효하지_않은_게임횟수_예외처리(int totalTimes){
+	@ValueSource(ints = {0, -1, -3436535})
+	public void 유효하지_않은_게임횟수_예외처리(int totalTimes) {
 
-		assertThatThrownBy(()->{
+		assertThatThrownBy(() -> {
 			GameTimes gameTimes = new GameTimes(totalTimes);
 		}).isInstanceOf(IllegalArgumentException.class);
 	}
 
 	@Test
-	public void 게임_시도횟수(){
-		int totalTimes=5;
+	public void 게임_시도횟수() {
+		int totalTimes = 5;
 		GameTimes gameTimes = new GameTimes(totalTimes);
 
-		int tryTimes=2;
-		for(int i=0; i<tryTimes; i++){
+		int tryTimes = 2;
+		for (int i = 0; i < tryTimes; i++) {
 			gameTimes.addTryTimes();
 		}
 
@@ -40,21 +40,21 @@ public class GameTimesTest {
 	}
 
 	@Test
-	public void 게임_시도끝_체크(){
-		int totalTimes=10;
+	public void 게임_시도끝_체크() {
+		int totalTimes = 10;
 		GameTimes gameTimes = new GameTimes(totalTimes);
-		for(int i=0; i<gameTimes.getTotalTimes(); i++){
+		for (int i = 0; i < gameTimes.getTotalTimes(); i++) {
 			gameTimes.addTryTimes();
 		}
 		assertThat(gameTimes.isGameFinish()).isTrue();
 	}
 
 	@Test
-	public void 게임_총횟수를_넘어_시도하면_예외처리(){
+	public void 게임_총횟수를_넘어_시도하면_예외처리() {
 		GameTimes gameTimes = new GameTimes(10);
 
-		assertThatThrownBy(()->{
-			for(int i=0; i<gameTimes.getTotalTimes()+1; i++){
+		assertThatThrownBy(() -> {
+			for (int i = 0; i < gameTimes.getTotalTimes() + 1; i++) {
 				gameTimes.addTryTimes();
 			}
 		}).isInstanceOf(IllegalStateException.class);
