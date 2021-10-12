96cab6675e2f56f63262bd38f1e4cb73cae2856d
jerry92k <jens92.k@gmail.com>
2021-10-12 19:54:45 +0900

    refactor(InvalidInputCommandException) : 유효하지 않은 사용자 입력값 예외 처리 클래스 정의

---
+++ src/main/java/racinggame/exception/InvalidInputCommandException.java
@@ -0,0 +1,16 @@
+package racinggame.exception;
+
+public class InvalidInputCommandException extends RuntimeException {
+
+	/**
+	 * @author Kim Jihee
+	 * @version 1.0
+	 * @since 1.0
+	 *
+	 * 사용자가 잘못된 값을 입력할 경우 발생하는 오류
+	 */
+
+	public InvalidInputCommandException(String message) {
+		super("[ERROR] : "+message);
+	}
+}
