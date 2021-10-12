adaf0b3e7f585a8388a0e2688572c7832e0094e4
jerry92k <jens92.k@gmail.com>
2021-10-12 19:55:34 +0900

    refactor(CarName) : 콘솔 출력 메세지 상수화

--- src/main/java/racinggame/domain/car/CarName.java
+++ src/main/java/racinggame/domain/car/CarName.java
@@ -1,22 +1,28 @@
 package racinggame.domain.car;
 
+import java.util.NoSuchElementException;
+
+import racinggame.exception.InvalidInputCommandException;
+
 /**
  * CarName 객체는 외부에 노출하지 않고 RacingCar 객체에 aggregation 형태로 사용
  */
 class CarName {
-	private final int CAR_NAME_MIN_LENGTH=1;
-	private final int CAR_NAME_MAX_LENGTH=5;
+	private final String CAR_NAME_ERROR_MESSAGE="[ERROR] 유효하지 않은 자동차 이름입니다. 이름의 길이는 1~5 사이 입니다.";
+	private final int CAR_NAME_MIN_LENGTH = 1;
+	private final int CAR_NAME_MAX_LENGTH = 5;
 
 	private String name;
 
 	public CarName(String name) {
 		validateCarName(name);
-		this.name=name;
+		this.name = name;
 	}
 
 	private void validateCarName(String carName) {
 		if (carName.length() < CAR_NAME_MIN_LENGTH || carName.length() > CAR_NAME_MAX_LENGTH) {
-			throw new IllegalArgumentException();
+			System.out.println(CAR_NAME_ERROR_MESSAGE);
+			throw new InvalidInputCommandException(CAR_NAME_ERROR_MESSAGE);
 		}
 
 	}
